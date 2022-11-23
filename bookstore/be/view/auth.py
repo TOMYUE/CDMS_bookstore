42525beed7711304a09c60a1f2e50b1373c11dc1
Caroline <87892579+Caroline-zou@users.noreply.github.com>
2022-11-23 21:20:31 +0800

    Update auth.py

--- bookstore/be/view/auth.py
+++ bookstore/be/view/auth.py
@@ -4,6 +4,8 @@ from fastapi.responses import JSONResponse
 from be.model import user
 from pydantic import BaseModel
 import sqlalchemy
+from typing import *
+from be.relations import user
 
 app = FastAPI()
 
@@ -12,13 +14,21 @@ class LoginForm(BaseModel):
     password: str
     terminal: str
 
-@app.post("/login")
-def login(form: LoginForm):
-    u = user.User()
-    code, message, token = u.login(
-        user_id=form.user_id, 
-        password=form.password, 
-        terminal=form.terminal
+@app.post("/seller/login")
+def seller_login(form: LoginForm):
+    code, message, token = user.seller_login(
+        form.user_id,
+        form.password,
+        form.terminal
+    )
+    return JSONResponse({"message": message, "token": token}, status_code=code)
+
+@app.post("/buyer/login")
+def buyer_login(form: LoginForm):
+    code, message, token = user.buyer_login(
+        form.user_id,
+        form.password,
+        form.terminal
     )
     return JSONResponse({"message": message, "token": token}, status_code=code)
 
@@ -27,12 +37,19 @@ class LogoutForm(BaseModel):
     user_id: int
     token: str
 
-@app.post("/logout")
-def logout(form: LogoutForm):
-    u = user.User()
-    code, message = u.logout(
-        user_id=user_id,
-        token=token
+@app.post("/seller/logout")
+def seller_logout(form: LogoutForm):
+    code, message = user.seller_logout(
+        user_id,
+        token
+    )
+    return JSONResponse({"message":message},status_code=code)
+
+@app.post("/buyer/logout")
+def buyer_logout(form: LogoutForm):
+    code, message = user.buyer_logout(
+        user_id,
+        token
     )
     return JSONResponse({"message":message},status_code=code)
 
@@ -42,13 +59,21 @@ class RegisterForm(BaseModel):
     uname: str
     password: str
 
-@app.post("/register")
-def register(form:RegisterForm):
-    u = user.User()
-    code, message = u.register(
-        user_id=form.user_id,
-        uname=form.uname,
-        password=form.password
+@app.post("/seller/register")
+def seller_register(form:RegisterForm):
+    code, message = user.seller_register(
+        form.user_id,
+        form.uname,
+        form.password
+    )
+    return JSONResponse({"message":message}, status_code=code)
+
+@app.post("/buyer/register")
+def buyer_register(form:RegisterForm):
+    code, message = user.buyer_register(
+        form.user_id,
+        form.uname,
+        form.password
     )
     return JSONResponse({"message":message}, status_code=code)
 
@@ -57,12 +82,19 @@ class UnregisterForm(BaseModel):
     user_id: int
     password: str
 
-@app.post("/unregister")
-def unregister(form:UnregisterForm):
-    u = user.User()
-    code, message = u.unregister(
-        user_id=user_id,
-        password=password
+@app.post("/seller/unregister")
+def seller_unregister(form:UnregisterForm):
+    code, message = user.seller_unregister(
+        user_id,
+        password
+    )
+    return JSONResponse({"message":message},status_code=code)
+
+@app.post("/buyer/unregister")
+def seller_unregister(form:UnregisterForm):
+    code, message = user.buyer_unregister(
+        user_id,
+        password
     )
     return JSONResponse({"message":message},status_code=code)
 
@@ -71,12 +103,20 @@ class ChangePwdForm(BaseModel):
     old_password: str
     new_password: str
 
-@app.post("/password")
+@app.post("/seller/password")
 def change_password(form: ChangePwdForm):
-    u = user.User()
-    code, message = u.change_password(
-        user_id=user_id,
+    code, message = user.seller_change_password(
+        user_id,
+        old_password,
+        new_password
+    )
+    return JSONResponse({"message":message},status_code=code)
+
+@app.post("/buyer/password")
+def change_password(form: ChangePwdForm):
+    code, message = user.buyer_change_password(
+        user_id,
         old_password=old_password,
-        new_password=new_password
+        new_password
     )
     return JSONResponse({"message":message},status_code=code)
