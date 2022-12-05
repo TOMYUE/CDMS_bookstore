52c72331c5ca6abb61d323076d73c1b4f2c226cc
Caroline-zou <1993808103@qq.com>
2022-12-05 23:46:26 +0800

    change uid to uname;update test_register

--- bookstore/be/relations/user.py
+++ bookstore/be/relations/user.py
@@ -24,17 +24,21 @@ def jwt_decode(encoded_token, user_id: str) -> str:
     return decoded
 
 
-def seller_login(uid: int, pwd: str, terminal: str):
+def seller_login(uname: str, pwd: str, terminal: str):
     try:
         with db_session() as session:
             token = ""
-            user = session.query(Seller).filter(Seller.uid == uid).first()
+            user = session.query(Seller).filter(Seller.uname == uname).first()
             # user = self.session.execute("SELECT pwd FROM Seller WHERE uid=:uid",{"uid":uid}).fetchone()
-            if user is None or pwd != user.pwd:
+            if user is None :
+                code, message = error.error_non_exist_user_id(uname)
+                return code, message, token
+            if pwd != user.pwd:
                 code, message = error.error_authorization_fail()
                 return code, message, token
-            token = jwt_encode(uid, terminal)
-            user = session.query(Seller).filter(Seller.uid == uid).update(token)
+            # token = jwt_encode("token", terminal)
+            token = "token"
+            user = session.query(Seller).filter(Seller.uname == uname).update({"token" : token})
             session.commit()
             # self.session.execute("UPDATE Seller set token= '%s' , terminal = '%s' where uid = '%s'" % (token, terminal, uid))
             # self.session.commit()
@@ -43,17 +47,20 @@ def seller_login(uid: int, pwd: str, terminal: str):
         return 500, f"Failure: {e}", token
 
 
-def buyer_login(uid: int, pwd: str, terminal: str):
+def buyer_login(uname: str, pwd: str, terminal: str):
     try:
         with db_session() as session:
             token = ""
-            user = session.query(Buyer).filter(Buyer.uid == uid).first()
+            user = session.query(Buyer).filter(Buyer.uname == uname).first()
             # user = self.session.execute("SELECT pwd FROM Buyer WHERE uid=:uid",{"uid":uid}).fetchone()
-            if user is None or pwd != user.pwd:
+            if user is None:
+                code, message = error.error_non_exist_user_id(uname)
+                return code, message, token
+            if pwd != user.pwd:
                 code, message = error.error_authorization_fail()
                 return code, message, token
-            token = jwt_encode(uid, terminal)
-            user = session.query(Buyer).filter(Buyer.uid == uid).update(token)
+            token = "token"
+            user = session.query(Buyer).filter(Buyer.uname == uname).update({"token" : token})
             # self.session.execute("UPDATE Buyer set token= '%s' , terminal = '%s' where uid = '%s'" % (token, terminal, uid))
             session.commit()
             return 200, "ok", token
@@ -61,35 +68,35 @@ def buyer_login(uid: int, pwd: str, terminal: str):
         return 500, f"Failure: {e}", token
 
 
-def seller_logout(uid: int, token: str):
+def seller_logout(uname: str, token: str):
     try:
         with db_session() as session:
-            user = session.query(Seller).filter(Seller.uid == uid).first()
+            user = session.query(Seller).filter(Seller.uname == uname).first()
             # user = self.session.execute("SELECT token from Seller WHERE uid='%s'"%(uid)).fetchone()
-            if user is None:
-                return error.error_authorization_fail()
+            # if user is None:  #这种情况不存在
+            #     return error.error_non_exist_user_id(uname)
             if user.token != token:
                 return error.error_authorization_fail()
-            newtoken = ""
+            newtoken = "token"
             # session.execute("UPDATE Seller SET token='%s' WHERE uid='%s'" %(newtoken, uid))
-            user = session.query(Seller).filter(Seller.uid == uid).update(newtoken)
+            user = session.query(Seller).filter(Seller.uname == uname).update({"token": newtoken})
             session.commit()
             return 200, "ok"
     except Exception as e:
         return 500, f"Failure: {e}"
 
 
-def buyer_logout(uid: int, token: str):
+def buyer_logout(uname: str, token: str):
     try:
         with db_session() as session:
-            user = session.query(Buyer).filter(Buyer.uid == uid).first()
+            user = session.query(Buyer).filter(Buyer.uname == uname).first()
             # user = self.session.execute("SELECT token from Seller WHERE uid='%s'"%(uid)).fetchone()
-            if user is None:
-                return error.error_authorization_fail()
+            # if user is None: #情况不存在
+            #     return error.error_authorization_fail()
             if user.token != token:
                 return error.error_authorization_fail()
-            newtoken = ""
-            user = session.query(Buyer).filter(Buyer.uid == uid).update(newtoken)
+            newtoken = "token"
+            user = session.query(Buyer).filter(Buyer.uname == uname).update({"token": newtoken})
             # session.execute("UPDATE Seller SET token='%s' WHERE uid='%s'" % (newtoken, uid))
             session.commit()
             return 200, "ok"
@@ -100,14 +107,19 @@ def buyer_logout(uid: int, token: str):
 def seller_register(uname: str, pwd: str, account: str, balance: float, token: str, terminal: str):
     try:
         with db_session() as session:
-            session.add(Seller(uname=uname,
-                               pwd=pwd,
-                               account=account,
-                               balance=balance,
-                               token=token,
-                               terminal=terminal))
-            session.commit()
-            return 200, "ok"#, seller.uid
+            user = session.query(Seller).filter(Seller.uname == uname).first()
+            if user is None:
+                session.add(Seller(uid=int(time.time()),
+                                    uname=uname,
+                                   pwd=pwd,
+                                   account=account,
+                                   balance=balance,
+                                   token=token,
+                                   terminal=terminal))
+                session.commit()
+                return 200, "ok"#, seller.uid
+            else:
+                return error.error_exist_user_id(uname)
     except Exception as e:
         return 500, f"Failure: {e}"#, None
 
@@ -116,78 +128,88 @@ def buyer_register(uname: str, pwd: str, account: str, balance: float, token: st
     terminal = "terminal_{}".format(str(time.time()))
     try:
         with db_session() as session:
-            session.add(Buyer(uname=uname,
-                              pwd=pwd,
-                              account=account,
-                              balance=balance,
-                              token=token,
-                              terminal=terminal))
-            session.commit()
-            return 200, "ok"
+            user = session.query(Buyer).filter(Buyer.uname == uname).first()
+            if user is None:
+                session.add(Buyer(uname=uname,
+                                  pwd=pwd,
+                                  account=account,
+                                  balance=balance,
+                                  token=token,
+                                  terminal=terminal))
+                session.commit()
+                return 200, "ok"
+            else:
+                return error.error_exist_user_id(uname)
     except Exception as e:
         return 500, f"Failure: {e}"
 
 
-def seller_unregister(uid: int, pwd: str):
+def seller_unregister(uname: str, pwd: str):
     try:
         with db_session() as session:
             # user = session.execute("SELECT pwd FROM  Seller WHERE uid=:uid",{"uid": uid}).fetchone()
-            user = session.query(Seller).filter(Seller.uid == uid).first()
+            user = session.query(Seller).filter(Seller.uname == uname).first()
             if user is None:
-                code, message = error.error_authorization_fail()
+                code, message = error.error_non_exist_user_id(uname)
                 return code, message
             if pwd != user.pwd:
                 code, message = error.error_authorization_fail()
                 return code, message
             # session.execute("DELETE from Seller WHERE uid='%s'" %(uid))
-            user = session.query(Seller).filter(Seller.uid == uid).delete()
+            user = session.query(Seller).filter(Seller.uname == uname).delete()
             session.commit()
             return 200, "ok"
     except Exception as e:
         return 500, f"Failure: {e}"
 
 
-def buyer_unregister(uid: int, pwd: str):
+def buyer_unregister(uname: str, pwd: str):
     try:
         with db_session() as session:
-            user = session.query(Buyer).filter(Buyer.uid == uid).first()
+            user = session.query(Buyer).filter(Buyer.uname == uname).first()
             if user is None:
-                code, message = error.error_authorization_fail()
+                code, message = error.error_non_exist_user_id(uname)
                 return code, message
             if pwd != user.pwd:
                 code, message = error.error_authorization_fail()
                 return code, message
             # session.execute("DELETE from Seller WHERE uid='%s'" %(uid))
-            user = session.query(Buyer).filter(Buyer.uid == uid).delete()
+            user = session.query(Buyer).filter(Buyer.uname == uname).delete()
             session.commit()
             return 200, "ok"
     except Exception as e:
         return 500, f"Failure: {e}"
 
 
-def seller_change_password(uid: int, old_password: str, new_password: str):
+def seller_change_password(uname: str, old_password: str, new_password: str):
     try:
         with db_session() as session:
-            user = session.query(Seller).filter(Seller.uid == uid).first()
-            if user is None or old_password != user.pwd:
+            user = session.query(Seller).filter(Seller.uname == uname).first()
+            if user is None:
+                code, message = error.error_non_exist_user_id(uname)
+                return code, message
+            if old_password != user.pwd:
                 code, message = error.error_authorization_fail()
                 return code, message
             # self.session.execute("UPDATE Seller SET pwd='%s' WHERE uid='%s'" %(new_password, uid))
-            res = session.query(Seller).filter(Seller.uid == uid).update(Seller.pwd)
+            res = session.query(Seller).filter(Seller.uname == uname).update({"pwd":new_password})
             session.commit()
             return 200, "ok"
     except Exception as e:
         return 500, f"Failure: {e}"
 
 
-def buyer_change_password(uid: int, old_password: str, new_password: str):
+def buyer_change_password(uname: str, old_password: str, new_password: str):
     try:
         with db_session() as session:
-            user = session.query(Buyer).filter(Buyer.uid == uid).first()
-            if user is None or old_password != user.pwd:
+            user = session.query(Buyer).filter(Buyer.uname == uname).first()
+            if user is None:
+                code, message = error.error_non_exist_user_id(uname)
+                return code, message
+            if old_password != user.pwd:
                 code, message = error.error_authorization_fail()
                 return code, message
-            res = session.query(Buyer).filter(Buyer.uid == uid).update(Buyer.pwd)
+            res = session.query(Buyer).filter(Buyer.uname == uname).update({"pwd":new_password})
             session.commit()
             return 200, "ok"
     except Exception as e:
--- bookstore/fe/test/test_register.py
+++ bookstore/fe/test/test_register.py
@@ -5,7 +5,9 @@ import pytest
 
 from fe.access import auth
 from fe import conf
-
+from be.relations.seller import *
+from be.relations.buyer import *
+from be.relations.user import *
 from typing import *
 
 class TestRegister:
@@ -13,36 +15,156 @@ class TestRegister:
     def pre_run_initialization(self):
         self.auth = auth.Auth(conf.URL)
         self.user_id = int(uuid.uuid1())
-        self.uname = "test_register"
-        self.password = "test_register_" + str(self.user_id)
+        self.uname = "test_register1"
+        self.password = "test_register_1"
         self.account = "test_register_" + str(self.user_id)
         self.balance = round(random.uniform(1000, 10000), 6)
+        self.token = "token"
+        self.terminal = "terminal_{}".format(str(time.time()))
         yield
 
-    def test_register_ok(self):
-        code = self.auth.register_buyer(self.user_id, self.password, self.uname, self.account, self.balance)
+    def test_buyer_register(self):
+        code, msg = buyer_register(self.uname, self.password,self.account, self.balance, self.token, self.terminal)
         assert code == 200
 
-    def test_unregister_ok(self):
-        code = self.auth.register_buyer(self.user_id, self.password, self.uname, self.account, self.balance)
+    def test_seller_register(self):
+        code, msg = seller_register(self.uname, self.password,self.account, self.balance, self.token, self.terminal)
         assert code == 200
 
-        code = self.auth.buyer_unregister(self.user_id, self.password)
+    #user exists already
+    def test_buyer_register_error(self):
+        code, msg = buyer_register(self.uname, "newpassword",self.account, self.balance, self.token, self.terminal)
+        assert code == 512
+
+    #user exists already
+    def test_seller_register_error(self):
+        code, msg = seller_register(self.uname, "newpassword",self.account, self.balance, self.token, self.terminal)
+        assert code == 512
+    def test_seller_login_ok(self):
+        code, msg, token = seller_login(self.uname, self.password, self.terminal)
+        print(msg)
         assert code == 200
 
-    def test_unregister_error_authorization(self):
-        code = self.auth.register_buyer(self.user_id, self.password, self.uname, self.account, self.balance)
+    #user not exist
+    def test_seller_login_error1(self):
+        code, msg, token = seller_login("nobody", self.password, self.terminal)
+        assert code == 511
+
+    #input wrong password
+    def test_seller_login_error2(self):
+        code, msg, token = seller_login(self.uname, "self.password", self.terminal)
+        print(msg)
+        assert code == 401
+
+    def test_buyer_login_ok(self):
+        code, msg, token = buyer_login(self.uname, self.password, self.terminal)
+        print(msg)
         assert code == 200
 
-        code = self.auth.buyer_unregister(self.user_id, self.password)
-        assert code != 200
+    # user not exist
+    def test_buyer_login_error1(self):
+        code, msg, token = buyer_login("nobody", self.password, self.terminal)
+        assert code == 511
 
-        code = self.auth.buyer_unregister(self.user_id, self.password + "_x")
-        assert code != 200
+    # input wrong password
+    def test_buyer_login_error2(self):
+        # print(self.password)
+        code, msg, token = buyer_login(self.uname, "self.password", self.terminal)
+        print(msg)
+        assert code == 401
 
-    def test_register_error_exist_user_id(self):
-        code = self.auth.register_buyer(self.user_id, self.password, self.uname, self.account, self.balance)
+    def test_seller_logout_ok(self):
+        code, msg = seller_logout(self.uname, self.token)
         assert code == 200
 
-        code = self.auth.register_buyer(self.user_id, self.password, self.uname, self.account, self.balance)
-        assert code != 200
+    #error token
+    def test_seller_logout_error(self):
+        code, msg = seller_logout(self.uname,"unknown")
+        assert code == 401
+
+    def test_buyer_logout_ok(self):
+        code, msg = buyer_logout(self.uname, self.token)
+        assert code == 200
+
+    #error token
+    def test_buyer_logout_error(self):
+        code, msg = buyer_logout(self.uname,"unknown")
+        assert code == 401
+
+    #user none exists
+    def test_seller_change_pwd_error1(self):
+        code, msg = seller_change_password("nobody", self.password, "test_register_2")
+        assert code == 511
+    #wrong password
+    def test_seller_change_pwd_error2(self):
+        code, msg = seller_change_password(self.uname, "mima", "test_register_2")
+        assert code == 401
+
+    def test_seller_change_pwd_ok(self):
+        code, msg = seller_change_password(self.uname, self.password,"test_register_1")
+        assert code == 200
+
+    # user none exists
+    def test_buyer_change_pwd_error1(self):
+        code, msg = buyer_change_password("nobody", self.password, "test_register_2")
+        assert code == 511
+
+    # wrong password
+    def test_buyer_change_pwd_error2(self):
+        code, msg = buyer_change_password(self.uname, "mima", "test_register_2")
+        assert code == 401
+
+    def test_buyer_change_pwd_ok(self):
+        code, msg = buyer_change_password(self.uname, self.password, "test_register_1")
+        assert code == 200
+
+    # user none exists
+    def test_seller_unregister_error1(self):
+        code, msg = seller_unregister("nobody", self.password)
+        assert code == 511
+
+    # wrong password
+    def test_seller_unregister_error2(self):
+        code, msg = seller_unregister(self.uname, "mima")
+
+
+
+    def test_seller_unregister_ok(self):
+        code, msg = seller_unregister(self.uname, self.password)
+        assert code == 200
+    # user none exists
+    def test_buyer_unregister_error1(self):
+        code, msg = buyer_unregister("nobody", self.password)
+        assert code == 511
+
+    # wrong password
+    def test_buyer_unregister_error2(self):
+        code, msg = buyer_unregister(self.uname, "mima")
+        assert code == 401
+
+    def test_buyer_unregister_ok(self):
+        code, msg = buyer_unregister(self.uname, self.password)
+        assert code == 200
+    # def test_unregister_ok(self):
+    #     code = self.auth.register_buyer(self.user_id, self.password, self.uname, self.account, self.balance)
+    #     assert code == 200
+    #
+    #     code = self.auth.buyer_unregister(self.user_id, self.password)
+    #     assert code == 200
+
+    # def test_unregister_error_authorization(self):
+    #     code = self.auth.register_buyer(self.user_id, self.password, self.uname, self.account, self.balance)
+    #     assert code == 200
+    #
+    #     code = self.auth.buyer_unregister(self.user_id, self.password)
+    #     assert code != 200
+    #
+    #     code = self.auth.buyer_unregister(self.user_id, self.password + "_x")
+    #     assert code != 200
+    #
+    # def test_register_error_exist_user_id(self):
+    #     code = self.auth.register_buyer(self.user_id, self.password, self.uname, self.account, self.balance)
+    #     assert code == 200
+    #
+    #     code = self.auth.register_buyer(self.user_id, self.password, self.uname, self.account, self.balance)
+    #     assert code != 200
