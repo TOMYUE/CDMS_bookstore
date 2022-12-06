a47de1ebb8ba7faecfc49b4ad2dc0d7b4f8bd9af
Andy Yang <71013200+Y-jiji@users.noreply.github.com>
2022-12-06 16:54:28 +0800

    Add: pytests

---
+++ bookstore/be/tests/test_auth.py
@@ -0,0 +1,38 @@
+from fastapi.testclient import TestClient
+from run import app
+from uuid import uuid1
+
+class SellerRequest:
+    def __init__(self) -> None:
+        self.cli = TestClient(app)
+
+    def register(self, expected_code=200) -> dict:
+        json = {
+            "user_id": int(uuid1()),
+            "uname": str(uuid1()),
+            "password": str(uuid1())
+        }
+        response = self.cli.post("/auth/seller/register", json=json)
+        assert response.status_code == expected_code
+        return json
+
+    def login(self, user_id, uname, password, expected_code=200) -> dict:
+        response = self.cli.post("/auth/seller/login")
+        assert response.status_code == expected_code
+        return {
+            "user_id": user_id,
+            "uname": uname,
+            "password": password
+        }
+
+    def logout():
+        raise Exception("TODO")
+
+    def change_pwd():
+        raise Exception("TODO")
+
+#** write tests here, utilize seller request **#
+
+def test_login_200():
+    SellerRequest().register(200)
+
